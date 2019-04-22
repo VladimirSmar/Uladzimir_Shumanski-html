ea633291eadb68fead07ab5e5f1dc1d58cf5793c
Uladzimir Shumanski <Shumaher160@gmail.com>
2019-04-22 15:30:50 +0300

    js-task3 fix

--- js-task3/builterPattern.js
+++ js-task3/builterPattern.js
@@ -15,18 +15,34 @@ class Structure {
 
 }
 
-function askForDetails() {
+class BuildingConsultant {
 
-  this.typeOfRoof = +prompt('What type of roof would your like to build? \n Gable - 1, Mansard - 2, Flat - 3', 1);
-  this.amountOfFloors = +prompt('How many floors would you like to add? \n Max amount of floors - 5', 1);
-  this.amountOfWindows = +prompt('How many windows would you like to add? \n Max amount of windows per floor - 4', 1);
-  this.amountOfIntrances = +prompt('How many intrances would you like to add? \n Max amount of intrances - 2', 1);
+  askForTypeOfRoof() {
+    var typeOfRoof = +prompt('What type of roof would your like to build? \n Gable - 1, Mansard - 2, Flat - 3', 1);
+    return typeOfRoof;
+  }
+
+  askForAmountOfFloors() {
+    var amountOfFloors = +prompt('How many floors would you like to add? \n Max amount of floors - 5', 1);
+    return amountOfFloors;
+  }
+
+  askForAmountOfWindows() {
+    var amountOfWindows = +prompt('How many windows would you like to add? \n Max amount of windows per floor - 4', 1);
+    return amountOfWindows;
+  }
+
+  askForAmountOfIntrances() {
+    var amountOfIntrances = +prompt('How many intrances would you like to add? \n Max amount of intrances - 2', 1);
+    return amountOfIntrances;
+  }
 
 }
 
 class StructureBuilder {
 
   constructor() {
+    this.validatorFunctions = new Validator();
     this.roof = null;
     this.floor = null;
     this.window = null;
@@ -34,22 +50,22 @@ class StructureBuilder {
   }
 
   buildRoof(typeOfRoof) {
-    this.roof = checkTypeOfRoofValue(typeOfRoof);
+    this.roof = this.validatorFunctions.checkTypeOfRoofValue(typeOfRoof);
     return this;
   }
 
   buildFloor(amountOfFloors) {
-    this.floor = checkAmountOfFloorsValue(amountOfFloors);
+    this.floor = this.validatorFunctions.checkAmountOfFloorsValue(amountOfFloors);
     return this;
   }
 
   buildWindow(amountOfWindows) {
-    this.window = checkAmountOfWindowsValue(amountOfWindows);
+    this.window = this.validatorFunctions.checkAmountOfWindowsValue(amountOfWindows);
     return this;
   }
 
   buildIntrance(amountOfIntrances) {
-    this.intrance = checkAmountOfIntrancesValue(amountOfIntrances);
+    this.intrance = this.validatorFunctions.checkAmountOfIntrancesValue(amountOfIntrances);
     return this;
   }
 
@@ -60,92 +76,72 @@ class StructureBuilder {
 
 }
 
-function checkTypeOfRoofValue(typeOfRoof) {
+class Validator {
 
-  if (typeOfRoof < 1 || typeOfRoof > 3) {
-    console.log('Wrong type of roof, default type was used ( Flat )');
-    return ROOF_TYPES.FLAT;
-  } else if (typeOfRoof === 1) {
-      return ROOF_TYPES.GABLE;
-  } else if (typeOfRoof === 2) {
-      return ROOF_TYPES.MANSARD;
-  } else if (typeOfRoof === 3) {
+  checkTypeOfRoofValue(typeOfRoof) {
+
+    if (typeOfRoof < 1 || typeOfRoof > 3) {
+      console.log('Wrong type of roof, default type was used ( Flat )');
       return ROOF_TYPES.FLAT;
+    } else if (typeOfRoof === 1) {
+        return ROOF_TYPES.GABLE;
+    } else if (typeOfRoof === 2) {
+        return ROOF_TYPES.MANSARD;
+    } else if (typeOfRoof === 3) {
+        return ROOF_TYPES.FLAT;
+    }
+
   }
 
-}
+  checkAmountOfFloorsValue(amountOfFloors) {
+    
+    if (amountOfFloors < 1 || amountOfFloors > 5) {
+      console.log("Wrong amount of floors, average amount of floors were used ( 3 )");
+      return 3;
+    } else {
+      return amountOfFloors;
+    }
 
-function checkAmountOfFloorsValue(amountOfFloors) {
-  
-  if (amountOfFloors < 1 || amountOfFloors > 5) {
-    console.log("Wrong amount of floors, average amount of floors were used ( 3 )");
-    return 3;
-  } else {
-    return amountOfFloors;
   }
 
-}
+  checkAmountOfWindowsValue(amountOfWindows) {
+    
+    if(amountOfWindows < 1 || amountOfWindows > 4) {
+      console.log("Wrong amount of windows, average amount of windows were used ( 3 )");
+      return 3;
+    } else {
+      return amountOfWindows;
+    }
 
-function checkAmountOfWindowsValue(amountOfWindows) {
-  
-  if(amountOfWindows < 1 || amountOfWindows > 4) {
-    console.log("Wrong amount of windows, average amount of windows were used ( 3 )");
-    return 3;
-  } else {
-    return amountOfWindows;
   }
 
-}
+  checkAmountOfIntrancesValue(amountOfIntrances) {
+    
+    if(amountOfIntrances < 1 || amountOfIntrances > 2) {
+      console.log("Wrong amount of intrances, average amount of intrances were used ( 1 )");
+      return 1;
+    } else {
+      return amountOfIntrances;
+    }
 
-function checkAmountOfIntrancesValue(amountOfIntrances) {
-  
-  if(amountOfIntrances < 1 || amountOfIntrances > 2) {
-    console.log("Wrong amount of intrances, average amount of intrances were used ( 1 )");
-    return 1;
-  } else {
-    return amountOfIntrances;
   }
 
-}
+  isObjectValid(object) {
 
-class SctucturingDirector {
+    if (typeof object == 'undefined') {
+      return false;
+    } else {
+      return true;
+    }
 
-  buildLittleStructure() {
-    var littleStructure = new StructureBuilder()
-        .buildRoof(1)
-        .buildFloor(1)
-        .buildWindow(1)
-        .buildIntrance(1)
-        .build();
-    structureRenderer(littleStructure);
   }
-  
-  buildMediumStructure() {
-    var mediumStructure = new StructureBuilder()
-        .buildRoof(2)
-        .buildFloor(3)
-        .buildWindow(3)
-        .buildIntrance(1)
-        .build();
-    structureRenderer(mediumStructure);
-  }
-  
-  buildBigStructure() {
-    var bigStructure = new StructureBuilder()
-        .buildRoof(3)
-        .buildFloor(5)
-        .buildWindow(4)
-        .buildIntrance(2)
-        .build();
-    structureRenderer(bigStructure);
-  }
-
 }
 
 function structureRenderer(structure) {
-  if( structure === null || structure === undefined || structure.roof === null && structure.floor === null && structure.window === null && structure.intrance === null) {
+  var objectValidator = new Validator();
+  if(!objectValidator.isObjectValid(structure)) {
     console.log('Sorry, we are not building structures anymore, now we are building crosses \u2629\u2629\u2629');
-  } else { 
+  } else {   
 
     if (structure.roof === ROOF_TYPES.GABLE) {
       console.log(
@@ -203,7 +199,6 @@ function structureRenderer(structure) {
         '\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588' +
         '\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\n');
     }
-// !                                                            !
     
     if (structure.window === 4) {
         for (var i = structure.floor; i > 0; i--) {
@@ -280,16 +275,11 @@ function structureRenderer(structure) {
 
 }
 
-askForDetails();
+var testBuildingConsultant = new BuildingConsultant();
 var testStructure = new StructureBuilder()
-    .buildRoof(this.typeOfRoof)
-    .buildFloor(this.amountOfFloors)
-    .buildWindow(this.amountOfWindows)
-    .buildIntrance(this.amountOfIntrances)
+    .buildRoof(testBuildingConsultant.askForTypeOfRoof())
+    .buildFloor(testBuildingConsultant.askForAmountOfFloors())
+    .buildWindow(testBuildingConsultant.askForAmountOfWindows())
+    .buildIntrance(testBuildingConsultant.askForAmountOfIntrances())
     .build();
 structureRenderer(testStructure);
-
-var testDirector = new SctucturingDirector();
-testDirector.buildLittleStructure();
-testDirector.buildMediumStructure();
-testDirector.buildBigStructure();
