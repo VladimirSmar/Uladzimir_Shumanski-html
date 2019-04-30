721c4bea241298c8a1e3db47e4c78f9be4de8d55
Uladzimir Shumanski <Shumaher160@gmail.com>
2019-04-30 15:32:16 +0300

    gitignore

--- js-task4/Backend/app.js
+++ js-task4/Backend/app.js
@@ -1,5 +1,5 @@
-const express = require('./node_modules/express');
-const request = require('./node_modules/request');
+const express = require('express');
+const request = require('request');
 const app = express();
 const port = 3000;
 
